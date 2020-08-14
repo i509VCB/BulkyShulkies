f1a590ddbf3d9b5619ee167b460e9d9902f33498
i509VCB <git@i509.me>
2020-08-14 00:13:09 -0500

    Begin on an ecs like system for shulker boxes

---
+++ src/main/java/me/i509/fabric/bulkyshulkies/ShulkerBoxType.java
@@ -0,0 +1,12 @@
+package me.i509.fabric.bulkyshulkies;
+
+public final class ShulkerBoxType {
+
+
+	private ShulkerBoxType() {
+	}
+
+	public static final class DataValue<V> {
+
+	}
+}
---
+++ src/main/java/me/i509/fabric/bulkyshulkies/data/ShulkerDataKeys.java
@@ -0,0 +1,8 @@
+package me.i509.fabric.bulkyshulkies.data;
+
+public final class ShulkerDataKeys {
+
+
+	private ShulkerDataKeys() {
+	}
+}
